24d2369a4a3258d6707792215e3f194857d81873
Abhilash Negi <tiger.abhi.3609@gmail.com>
2021-05-16 15:23:50 +0530

    functionality done without the stylying

--- Documents/Coding/Typescript/quiz-typescript/src/App.tsx
+++ Documents/Coding/Typescript/quiz-typescript/src/App.tsx
@@ -1,28 +1,85 @@
-import React from 'react'
+import React, { useState } from 'react'
 import QuestionCard from "./components/QuestionCard"
+import { fetchQuizQuestions, Difficulty, QuestionState } from "./apis"
+
+export type AnswerObject = {
+  question: string,
+  answer: string,
+  correct: boolean,
+  correctAnswer: string
+}
+
+const TOTAL_QUESTIONS = 10;
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [questions, setQuestions] = useState<QuestionState[]>([])
+  const [number, setNumber] = useState(0)
+  const [userAnswers, setUserAnswers] = useState<AnswerObject[]>([])
+  const [score, setScore] = useState(0)
+  const [gameOver, setGameOver] = useState(true)
 
   const startTrivia = async () => {
-
+    setIsLoading(true)
+    setGameOver(false)
+    try {
+      const newQuestions = await fetchQuizQuestions(TOTAL_QUESTIONS, Difficulty.EASY)
+      setQuestions(newQuestions)
+      setScore(0)
+      setUserAnswers([])
+      setNumber(0)
+    } catch (error) {
+      console.error(error)
+    }
+    setIsLoading(false)
   }
 
   const checkAnswer = (e: React.MouseEvent<HTMLButtonElement>) => {
-
+    if(!gameOver) {
+      const answer = e.currentTarget.value;
+      const correct = questions[number].correct_answer === answer
+      if(correct) setScore(prevScore => prevScore + 1)
+      const answerobj = {
+        question: questions[number].question,
+        answer,
+        correct,
+        correctAnswer: questions[number].correct_answer
+      }
+      setUserAnswers(prev => [...prev, answerobj])
+    }
   }
 
   const nextQuestion = () => {
-
+    const nextQuestion = number + 1
+    if(nextQuestion === TOTAL_QUESTIONS) {
+      setGameOver(true)
+    } else {
+      setNumber(nextQuestion)
+    }
   }
 
   return (
     <div className="App">
       <h1>Trivia</h1>
-      <button className="start" onClick={startTrivia}>Start Quiz</button>
-      <p className="score">Score:</p>
-      <p>Loading...</p>
-      <QuestionCard />
-      <button className="next" onClick={nextQuestion}>Next</button>
+      {gameOver || userAnswers.length === TOTAL_QUESTIONS ? <button className="start" onClick={startTrivia}>Start Quiz</button> : null}
+      {!gameOver && <p className="score">Score: {score}</p>}
+      {isLoading && <p>Loading...</p> }
+      {!isLoading && !gameOver && (
+        <QuestionCard 
+          question={questions[number].question}
+          answers={questions[number].answers}
+          callback={checkAnswer}
+          userAnswer={userAnswers ? userAnswers[number] : undefined}
+          questionNum={number + 1}
+          totalQuestions={TOTAL_QUESTIONS} 
+        />
+      )}
+      {!isLoading && !gameOver && userAnswers.length === number + 1 && number !== TOTAL_QUESTIONS - 1 
+        ? 
+        (<button className="next" onClick={nextQuestion}>Next</button>)
+        :
+        null
+      }
     </div>
   )
 }
--- Documents/Coding/Typescript/quiz-typescript/src/components/QuestionCard.tsx
+++ Documents/Coding/Typescript/quiz-typescript/src/components/QuestionCard.tsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { AnswerObject } from "../App"
 
-const QuestionCard = () => {
+type Props = {
+    question: string,
+    answers: string[],
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void,
+    userAnswer: AnswerObject | undefined,
+    questionNum: number,
+    totalQuestions: number
+}
+
+const QuestionCard: React.FC<Props> = ({ question, answers, callback, userAnswer, questionNum, totalQuestions }) => {
     return (
         <div>
-            
+            <p className="number">Question: {questionNum} / {totalQuestions}</p>
+            <p dangerouslySetInnerHTML={{__html: question}} />
+            <div>
+                {answers.map(answer => {
+                    return <button key={answers.indexOf(answer)} disabled={!!userAnswer} value={answer} onClick={callback}>{answer}</button>
+                })}
+            </div>
         </div>
     )
 }
